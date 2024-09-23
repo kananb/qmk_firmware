391448fa305d3f85bd1010820a19d5782eb9c331
Kanan Boubion <kananboubion@gmail.com>
2024-09-23 11:04:51 -0700

    Add OSM combos and move around caps word

--- keyboards/beekeeb/piantor/keymaps/stego/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/stego/keymap.c
@@ -3,21 +3,48 @@
 
 #define LAYOUT LAYOUT_split_3x6_3
 
+/* Custom keycodes */
+enum custom_keycodes {
+    CKC_CW_OFF = SAFE_RANGE,
+};
+
+
 /* Combos */
 enum combos {
     COMBO_OSL_FN,
     COMBO_TO_FN,
     COMBO_GUI,
+
+    COMBO_OSM_LCTL,
+    COMBO_OSM_LSHFT,
+    COMBO_OSM_LALT,
+    COMBO_OSM_RCTL,
+    COMBO_OSM_RSHFT,
+    COMBO_OSM_RALT,
 };
 
 const uint16_t PROGMEM osl_fun_combo[] = {OSL(1), KC_SPC, COMBO_END};
 const uint16_t PROGMEM to_fun_combo[] = {OSL(2), KC_ENT, COMBO_END};
 const uint16_t PROGMEM gui_combo[] = {OSL(2), KC_G, COMBO_END};
 
+const uint16_t PROGMEM osm_lctl_combo[] = {LCTL_T(KC_T), KC_P, COMBO_END};
+const uint16_t PROGMEM osm_lshft_combo[] = {LSFT_T(KC_S), KC_F, COMBO_END};
+const uint16_t PROGMEM osm_lalt_combo[] = {LALT_T(KC_R), KC_B, COMBO_END};
+const uint16_t PROGMEM osm_rctl_combo[] = {RCTL_T(KC_N), KC_L, COMBO_END};
+const uint16_t PROGMEM osm_rshft_combo[] = {RSFT_T(KC_E), KC_U, COMBO_END};
+const uint16_t PROGMEM osm_ralt_combo[] = {RALT_T(KC_I), KC_Y, COMBO_END};
+
 combo_t key_combos[] = {
     [COMBO_OSL_FN]  = COMBO(osl_fun_combo, OSL(3)),
     [COMBO_TO_FN] = COMBO(to_fun_combo, TO(3)),
     [COMBO_GUI] = COMBO(gui_combo, KC_LGUI),
+
+    [COMBO_OSM_LCTL] = COMBO(osm_lctl_combo, OSM(MOD_LCTL)),
+    [COMBO_OSM_LSHFT] = COMBO(osm_lshft_combo, OSM(MOD_LSFT)),
+    [COMBO_OSM_LALT] = COMBO(osm_lalt_combo, OSM(MOD_LALT)),
+    [COMBO_OSM_RCTL] = COMBO(osm_rctl_combo, OSM(MOD_RCTL)),
+    [COMBO_OSM_RSHFT] = COMBO(osm_rshft_combo, OSM(MOD_RSFT)),
+    [COMBO_OSM_RALT] = COMBO(osm_ralt_combo, OSM(MOD_RALT)),
 };
 
 
@@ -29,7 +56,7 @@ combo_t key_combos[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LAYER0] = LAYOUT(KC_NO, KC_NO, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_NO, KC_NO, KC_Q, KC_A, LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G, KC_M, RCTL_T(KC_N), RSFT_T(KC_E), RALT_T(KC_I), KC_O, KC_QUOT, KC_NO, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_NO, OSL(1), OSL(2), KC_ENT, KC_SPC, KC_NO),
-    [_LAYER1] = LAYOUT(KC_NO, KC_NO, KC_ESC, KC_DEL, KC_TAB, LGUI(KC_ESC), KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_NO, KC_NO, KC_UNDS, KC_1, LALT_T(KC_2), LSFT_T(KC_3), LCTL_T(KC_4), KC_5, KC_6, RCTL_T(KC_7), RSFT_T(KC_8), RALT_T(KC_9), KC_0, KC_NO, KC_NO, LCTL(KC_Z), LCTL(KC_Y), LCTL(KC_C), LCTL(KC_V), LGUI(KC_L), CW_TOGG, KC_BSPC, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_ENT, KC_TRNS, KC_NO),
+    [_LAYER1] = LAYOUT(KC_NO, KC_NO, KC_ESC, KC_DEL, KC_TAB, LGUI(KC_ESC), KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_NO, KC_NO, KC_UNDS, KC_1, LALT_T(KC_2), LSFT_T(KC_3), LCTL_T(KC_4), KC_5, KC_6, RCTL_T(KC_7), RSFT_T(KC_8), RALT_T(KC_9), KC_0, CW_TOGG, KC_NO, LCTL(KC_Z), LCTL(KC_Y), LCTL(KC_C), LCTL(KC_V), LGUI(KC_L), CKC_CW_OFF, KC_BSPC, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_ENT, KC_TRNS, KC_NO),
     [_LAYER2] = LAYOUT(KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_GRV, KC_BSLS, KC_PIPE, KC_DQUO, KC_ASTR, KC_SLSH, KC_NO, KC_NO, KC_UNDS, KC_DLR, KC_LPRN, KC_RPRN, LCTL_T(KC_SCLN), KC_AT, KC_AMPR, RCTL_T(KC_EQL), KC_PLUS, KC_MINS, KC_EXLM, KC_TILD, KC_NO, KC_PERC, KC_LCBR, KC_RCBR, KC_COLN, KC_CIRC, KC_HASH, KC_QUOT, KC_LT, KC_GT, KC_QUES, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO),
     [_LAYER3] = LAYOUT(KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_F10, KC_F7, KC_F8, KC_F9, KC_NO, KC_NO, TO(0), KC_LEFT, LALT_T(KC_DOWN), LSFT_T(KC_UP), LCTL_T(KC_RGHT), KC_PSCR, KC_F11, RCTL_T(KC_F4), RSFT_T(KC_F5), RALT_T(KC_F6), KC_NO, QK_BOOT, KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, KC_F12, KC_F1, KC_F2, KC_F3, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO) 
 };
@@ -40,6 +67,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     if (!process_achordion(keycode, record)) {
         return false;
+    }
+
+    switch (keycode) {
+        case CKC_CW_OFF:
+            if (record->event.pressed) {
+                caps_word_off();
+            }
+            return false;
     }
 
     return true; // Process all other keycodes normally
