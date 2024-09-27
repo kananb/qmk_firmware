475bdcb1afccc6128a9614729162894ce8521ab6
Kanan Boubion <kananboubion@gmail.com>
2024-09-27 16:05:36 -0700

    Update which keys end caps word

--- keyboards/beekeeb/piantor/keymaps/stego/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/stego/keymap.c
@@ -178,6 +178,17 @@ tap_dance_action_t tap_dance_actions[] = {
 };
 
 
+/* Leader key functions */
+
+void leader_start_user(void) {
+    // Do nothing
+}
+
+void leader_end_user(void) {
+    // Do nothing for now
+}
+
+
 /* QMK functions */
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
@@ -215,6 +226,28 @@ uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t* record) {
 
 bool get_retro_tapping(uint16_t keycode, keyrecord_t* record) {
     return !is_thumb_key(record);
+}
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+        case QK_LEAD:
+        case TD(LEAD_LAYER):
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
 }
 
 
