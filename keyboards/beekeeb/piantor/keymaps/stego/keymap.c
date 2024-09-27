dde88fa04adc779b59e09f97a6cb15c1c454a4c3
Kanan Boubion <kananboubion@gmail.com>
2024-09-27 15:28:17 -0700

    Configure tap dancing

--- keyboards/beekeeb/piantor/keymaps/stego/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/stego/keymap.c
@@ -6,9 +6,36 @@
 
 
 /* Custom keycodes */
+
 enum custom_keycodes {
     LLOCK = SAFE_RANGE,
 };
+
+
+/* Tap dance definitions */
+
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+enum td_keycodes {
+    LEAD_LAYER,
+    LOCK_COMPUTER,
+};
+
+td_state_t cur_dance(tap_dance_state_t *state);
+void ll_finished(tap_dance_state_t *state, void *user_data);
+void ll_reset(tap_dance_state_t *state, void *user_data);
+void lc_finished(tap_dance_state_t *state, void *user_data);
 
 
 /* Combos */
@@ -16,8 +43,6 @@ enum custom_keycodes {
 enum combos {
     COMBO_OSL_FN,
     COMBO_GUI,
-    COMBO_LLEAD,
-    COMBO_RLEAD,
 
     COMBO_OSM_LCTL,
     COMBO_OSM_LSHFT,
@@ -27,10 +52,8 @@ enum combos {
     COMBO_OSM_RALT,
 };
 
-const uint16_t PROGMEM osl_fun_combo[] = {OSL(1), LT(2,KC_SPC), COMBO_END};
-const uint16_t PROGMEM gui_combo[] = {OSL(1), KC_G, COMBO_END};
-const uint16_t PROGMEM llead_combo[] = {KC_T, KC_G, COMBO_END};
-const uint16_t PROGMEM rlead_combo[] = {KC_N, KC_M, COMBO_END};
+const uint16_t PROGMEM osl_fun_combo[] = {TD(LEAD_LAYER), LT(2,KC_SPC), COMBO_END};
+const uint16_t PROGMEM gui_combo[] = {TD(LEAD_LAYER), KC_G, COMBO_END};
 
 const uint16_t PROGMEM osm_lctl_combo[] = {LCTL_T(KC_T), KC_P, COMBO_END};
 const uint16_t PROGMEM osm_lshft_combo[] = {LSFT_T(KC_S), KC_F, COMBO_END};
@@ -42,8 +65,6 @@ const uint16_t PROGMEM osm_ralt_combo[] = {RALT_T(KC_I), KC_Y, COMBO_END};
 combo_t key_combos[] = {
     [COMBO_OSL_FN]  = COMBO(osl_fun_combo, OSL(3)),
     [COMBO_GUI] = COMBO(gui_combo, KC_LGUI),
-    [COMBO_LLEAD] = COMBO(llead_combo, QK_LEAD),
-    [COMBO_RLEAD] = COMBO(rlead_combo, QK_LEAD),
 
     [COMBO_OSM_LCTL] = COMBO(osm_lctl_combo, OSM(MOD_LCTL)),
     [COMBO_OSM_LSHFT] = COMBO(osm_lshft_combo, OSM(MOD_LSFT)),
@@ -62,10 +83,10 @@ combo_t key_combos[] = {
 #define _LAYER3 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_LAYER0] = LAYOUT(KC_NO, KC_NO, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_NO, KC_NO, KC_Q, LGUI_T(KC_A), LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G, KC_M, RCTL_T(KC_N), RSFT_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT, KC_NO, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_NO, OSL(1), KC_NO, KC_NO, LT(2,KC_SPC), KC_NO),
-    [_LAYER1] = LAYOUT(KC_NO, KC_NO, KC_ESC, KC_DEL, KC_TAB, LGUI(KC_L), KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_NO, KC_NO, KC_UNDS, LGUI_T(KC_1), LALT_T(KC_2), LSFT_T(KC_3), LCTL_T(KC_4), KC_5, KC_6, RCTL_T(KC_7), RSFT_T(KC_8), RALT_T(KC_9), RGUI_T(KC_0), CW_TOGG, KC_NO, LCTL(KC_Z), KC_ENT, LCTL(KC_C), LCTL(KC_V), LCTL(KC_Y), QK_LEAD, KC_BSPC, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO),
+    [_LAYER0] = LAYOUT(KC_NO, KC_NO, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_NO, KC_NO, KC_Q, KC_A, LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G, KC_M, RCTL_T(KC_N), RSFT_T(KC_E), RALT_T(KC_I), KC_O, KC_QUOT, KC_NO, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_NO, TD(LEAD_LAYER), KC_NO, KC_NO, LT(2,KC_SPC), KC_NO),
+    [_LAYER1] = LAYOUT(KC_NO, KC_NO, KC_ESC, KC_DEL, KC_TAB, TD(LOCK_COMPUTER), KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_NO, KC_NO, KC_UNDS, KC_1, LALT_T(KC_2), LSFT_T(KC_3), LCTL_T(KC_4), KC_5, KC_6, RCTL_T(KC_7), RSFT_T(KC_8), RALT_T(KC_9), KC_0, CW_TOGG, KC_NO, LCTL(KC_Z), KC_ENT, LCTL(KC_C), LCTL(KC_V), LCTL(KC_Y), QK_LEAD, KC_BSPC, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO),
     [_LAYER2] = LAYOUT(KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_GRV, KC_BSLS, KC_PIPE, KC_DQUO, KC_ASTR, KC_SLSH, KC_NO, KC_NO, KC_UNDS, KC_DLR, KC_LPRN, KC_RPRN, LCTL_T(KC_SCLN), KC_AT, KC_AMPR, RCTL_T(KC_EQL), KC_PLUS, KC_MINS, KC_EXLM, KC_TILD, KC_NO, KC_PERC, KC_LCBR, KC_RCBR, KC_COLN, KC_CIRC, KC_HASH, KC_QUOT, KC_LT, KC_GT, KC_QUES, KC_NO, KC_NO, KC_SPC, KC_NO, KC_NO, KC_TRNS, KC_NO),
-    [_LAYER3] = LAYOUT(KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_F10, KC_F7, KC_F8, KC_F9, KC_NO, KC_NO, LLOCK, LGUI_T(KC_LEFT), LALT_T(KC_DOWN), LSFT_T(KC_UP), LCTL_T(KC_RGHT), KC_PSCR, KC_F11, RCTL_T(KC_F4), RSFT_T(KC_F5), RALT_T(KC_F6), KC_RGUI, QK_BOOT, KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, KC_F12, KC_F1, KC_F2, KC_F3, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO)
+    [_LAYER3] = LAYOUT(KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_F10, KC_F7, KC_F8, KC_F9, KC_NO, KC_NO, LLOCK, KC_LEFT, LALT_T(KC_DOWN), LSFT_T(KC_UP), LCTL_T(KC_RGHT), KC_PSCR, KC_F11, RCTL_T(KC_F4), RSFT_T(KC_F5), RALT_T(KC_F6), OSM(MOD_RGUI), QK_BOOT, KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, KC_F12, KC_F1, KC_F2, KC_F3, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO)
 };
 
 
@@ -81,9 +102,80 @@ Side side_of_key(keyrecord_t* key_record) {
     uint8_t row = key_record->event.key.row;
 
     if (row <= 2) return LEFT;
+    else if (row == 3) return NONE;
     else if (row <= 6) return RIGHT;
     else return NONE;
 }
+
+bool is_thumb_key(keyrecord_t* key_record) {
+    uint8_t row = key_record->event.key.row;
+    return row == 7 || row == 3;
+}
+
+/* Tap dance functions */
+
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (!state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (!state->pressed) return TD_DOUBLE_TAP;
+        else return TD_UNKNOWN;
+    }
+    else return TD_UNKNOWN;
+}
+
+static td_tap_t ll_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+void ll_finished(tap_dance_state_t *state, void *user_data) {
+    ll_tap_state.state = cur_dance(state);
+    switch (ll_tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code16(QK_LEAD);
+            break;
+        case TD_SINGLE_HOLD:
+            layer_on(_LAYER1);
+            break;
+        default:
+            break;
+    }
+}
+
+void ll_reset(tap_dance_state_t *state, void *user_data) {
+    // If the key was held down and now is released then switch off the layer
+    if (ll_tap_state.state == TD_SINGLE_HOLD) {
+        layer_off(_LAYER1);
+    }
+    ll_tap_state.state = TD_NONE;
+}
+
+static td_tap_t lc_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+void lc_finished(tap_dance_state_t *state, void *user_data) {
+    lc_tap_state.state = cur_dance(state);
+    switch (lc_tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code16(LGUI(KC_L));
+            break;
+        case TD_DOUBLE_TAP:
+            tap_code16(LGUI(KC_ESC));
+            break;
+        default:
+            break;
+    }
+}
+
+// Associate our tap dance key with its functionality
+tap_dance_action_t tap_dance_actions[] = {
+    [LEAD_LAYER] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ll_finished, ll_reset),
+    [LOCK_COMPUTER] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lc_finished, NULL),
+};
 
 
 /* QMK functions */
@@ -100,9 +192,11 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t* record) {
     uint8_t col = record->event.key.col;
 
     if ((side == LEFT && col <= 1) || (side == RIGHT && col >= 4))
-        return TAPPING_TERM + 100; // Increase tapping term for pinky keys.
+        return TAPPING_TERM + 200; // Increase tapping term for pinky keys.
     else if ((side == LEFT && col == 2) || (side == RIGHT && col == 3))
         return TAPPING_TERM + 70; // Increase tapping term for ring keys.
+    else if (is_thumb_key(record))
+        return TAPPING_TERM + 30; // Increase tapping term for thumb keys.
     else return TAPPING_TERM; // Otherwise, force hold and disable key repeating.
 }
 
@@ -117,6 +211,10 @@ uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t* record) {
         default:
             return 0; // Otherwise disable quick taps.
     }
+}
+
+bool get_retro_tapping(uint16_t keycode, keyrecord_t* record) {
+    return !is_thumb_key(record);
 }
 
 
@@ -147,5 +245,5 @@ bool achordion_eager_mod(uint8_t mod) {
             return true; // Eagerly apply shift and control mods.
     }
 
-    return false; // Otherwise, apply mods on release.
+    return false;
 }
