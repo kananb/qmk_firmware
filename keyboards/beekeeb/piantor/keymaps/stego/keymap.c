c12ab721c8f89551782dad62b48ab2dbc4b6b4dc
Kanan Boubion <kananboubion@gmail.com>
2024-10-03 08:41:22 -0700

    Move to a 26 key layout

--- keyboards/beekeeb/piantor/keymaps/stego/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/stego/keymap.c
@@ -23,7 +23,7 @@ enum custom_keycodes {
 };
 
 enum td_keycodes {
-    LOCK_COMPUTER,
+    ESC_LOCK,
 };
 
 #define LL_LAYER SYM
@@ -39,6 +39,7 @@ typedef enum {
     TD_SINGLE_TAP,
     TD_SINGLE_HOLD,
     TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
 } td_state_t;
 
 typedef struct {
@@ -47,16 +48,22 @@ typedef struct {
 } td_tap_t;
 
 td_state_t cur_dance(tap_dance_state_t *state);
-void lc_finished(tap_dance_state_t *state, void *user_data);
-void lc_reset(tap_dance_state_t *state, void *user_data);
+void el_finished(tap_dance_state_t *state, void *user_data);
+void el_reset(tap_dance_state_t *state, void *user_data);
 
 
 /* Combos */
 
 enum combos {
     COMBO_J,
+    COMBO_Q,
     COMBO_X,
     COMBO_Z,
+
+    COMBO_UNDS,
+    COMBO_ASTR,
+    COMBO_F12,
+    // COMBO_ESC,
 
     COMBO_OSL_FN,
     COMBO_GUI,
@@ -65,10 +72,12 @@ enum combos {
     COMBO_RLEAD,
 
     COMBO_OSM_LCTL,
-    COMBO_OSM_LSHFT,
+    COMBO_OSM_LSHFT_A,
+    COMBO_OSM_LSHFT_S,
     COMBO_OSM_LALT,
     COMBO_OSM_RCTL,
-    COMBO_OSM_RSHFT,
+    COMBO_OSM_RSHFT_A,
+    COMBO_OSM_RSHFT_S,
     COMBO_OSM_RALT,
 
     COMBO_BSPC_LHS,
@@ -76,8 +85,14 @@ enum combos {
 };
 
 const uint16_t PROGMEM j_combo[] = {KC_F, KC_P, COMBO_END};
+const uint16_t PROGMEM q_combo[] = {KC_W, KC_F, COMBO_END};
 const uint16_t PROGMEM x_combo[] = {KC_V, KC_C, COMBO_END};
-const uint16_t PROGMEM z_combo[] = {KC_W, KC_F, COMBO_END};
+const uint16_t PROGMEM z_combo[] = {KC_C, KC_D, COMBO_END};
+
+const uint16_t PROGMEM unds_combo[] = {KC_H, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM astr_combo[] = {KC_PLUS, KC_MINS, COMBO_END};
+const uint16_t PROGMEM f12_combo[] = {KC_F1, KC_F2, COMBO_END};
+// const uint16_t PROGMEM esc_combo[] = {KC_W, KC_F, KC_P, COMBO_END};
 
 const uint16_t PROGMEM osl_fun_combo[] = {LTHUMB, RTHUMB, COMBO_END};
 const uint16_t PROGMEM gui_combo[] = {LTHUMB, KC_G, COMBO_END};
@@ -86,10 +101,12 @@ const uint16_t PROGMEM llead_combo[] = {LCTL_T(KC_T), KC_G, COMBO_END};
 const uint16_t PROGMEM rlead_combo[] = {RCTL_T(KC_N), KC_M, COMBO_END};
 
 const uint16_t PROGMEM osm_lctl_combo[] = {LCTL_T(KC_T), KC_P, COMBO_END};
-const uint16_t PROGMEM osm_lshft_combo[] = {LSFT_T(KC_S), KC_F, COMBO_END};
+const uint16_t PROGMEM osm_lshfta_combo[] = {LSFT_T(KC_S), KC_F, COMBO_END};
+const uint16_t PROGMEM osm_lshfts_combo[] = {KC_RBRC, KC_RPRN, COMBO_END};
 const uint16_t PROGMEM osm_lalt_combo[] = {LALT_T(KC_R), KC_W, COMBO_END};
 const uint16_t PROGMEM osm_rctl_combo[] = {RCTL_T(KC_N), KC_L, COMBO_END};
-const uint16_t PROGMEM osm_rshft_combo[] = {RSFT_T(KC_E), KC_U, COMBO_END};
+const uint16_t PROGMEM osm_rshfta_combo[] = {RSFT_T(KC_E), KC_U, COMBO_END};
+const uint16_t PROGMEM osm_rshfts_combo[] = {KC_ASTR, KC_PLUS, COMBO_END};
 const uint16_t PROGMEM osm_ralt_combo[] = {RALT_T(KC_I), KC_Y, COMBO_END};
 
 const uint16_t PROGMEM bspc_lhs_combo[] = {KC_W, KC_P, COMBO_END};
@@ -97,8 +114,14 @@ const uint16_t PROGMEM ent_lhs_combo[] = {KC_V, KC_D, COMBO_END};
 
 combo_t key_combos[] = {
     [COMBO_J] = COMBO(j_combo, KC_J),
+    [COMBO_Q] = COMBO(q_combo, KC_Q),
     [COMBO_X] = COMBO(x_combo, KC_X),
     [COMBO_Z] = COMBO(z_combo, KC_Z),
+
+    [COMBO_UNDS] = COMBO(unds_combo, KC_UNDS),
+    [COMBO_ASTR] = COMBO(astr_combo, KC_ASTR),
+    [COMBO_F12] = COMBO(f12_combo, KC_F12),
+    // [COMBO_ESC] = COMBO(esc_combo, KC_ESC),
     
     [COMBO_OSL_FN]  = COMBO(osl_fun_combo, OSL(NAV)),
     [COMBO_GUI] = COMBO(gui_combo, KC_LGUI),
@@ -107,23 +130,26 @@ combo_t key_combos[] = {
     [COMBO_RLEAD] = COMBO(rlead_combo, QK_LEAD),
 
     [COMBO_OSM_LCTL] = COMBO(osm_lctl_combo, OSM(MOD_LCTL)),
-    [COMBO_OSM_LSHFT] = COMBO(osm_lshft_combo, OSM(MOD_LSFT)),
+    [COMBO_OSM_LSHFT_A] = COMBO(osm_lshfta_combo, OSM(MOD_LSFT)),
+    [COMBO_OSM_LSHFT_S] = COMBO(osm_lshfts_combo, OSM(MOD_LSFT)),
     [COMBO_OSM_LALT] = COMBO(osm_lalt_combo, OSM(MOD_LALT)),
     [COMBO_OSM_RCTL] = COMBO(osm_rctl_combo, OSM(MOD_RCTL)),
-    [COMBO_OSM_RSHFT] = COMBO(osm_rshft_combo, OSM(MOD_RSFT)),
+    [COMBO_OSM_RSHFT_A] = COMBO(osm_rshfta_combo, OSM(MOD_RSFT)),
+    [COMBO_OSM_RSHFT_S] = COMBO(osm_rshfts_combo, OSM(MOD_RSFT)),
     [COMBO_OSM_RALT] = COMBO(osm_ralt_combo, OSM(MOD_RALT)),
 
     [COMBO_BSPC_LHS] = COMBO(bspc_lhs_combo, KC_BSPC),
     [COMBO_ENT_LHS] = COMBO(ent_lhs_combo, KC_ENT),
 };
 
+
 /* Key mapping */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_LAYER0] = LAYOUT(KC_NO, KC_NO, KC_W, KC_F, KC_P, KC_NO, KC_NO, KC_L, KC_U, KC_Y, KC_NO, KC_NO, KC_NO, KC_A, LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G, KC_M, RCTL_T(KC_N), RSFT_T(KC_E), RALT_T(KC_I), KC_O, KC_NO, KC_NO, KC_Q, KC_V, KC_C, KC_D, KC_B, KC_K, KC_H, KC_BSPC, KC_QUOT, KC_ENT, KC_NO, KC_NO, LTHUMB, KC_NO, KC_NO, RTHUMB, KC_NO),
-    [_LAYER1] = LAYOUT(KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_GRV, KC_NO, KC_NO, KC_BSLS, KC_ASTR, KC_SLSH, KC_NO, KC_NO, KC_NO, KC_DLR, KC_LPRN, KC_RPRN, LCTL_T(KC_SCLN), KC_AT, KC_AMPR, RCTL_T(KC_EQL), KC_PLUS, KC_MINS, KC_EXLM, KC_NO, KC_NO, KC_PERC, KC_LCBR, KC_RCBR, KC_UNDS, KC_HASH, KC_PIPE, KC_DQUO, KC_COMM, KC_DOT, KC_QUES, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO),
-    [_LAYER2] = LAYOUT(KC_NO, KC_NO, KC_ESC, KC_DEL, KC_TAB, KC_NO, KC_NO, KC_HOME, KC_PGUP, QK_DYNAMIC_MACRO_PLAY_1, KC_NO, KC_NO, KC_NO, KC_1, LALT_T(KC_2), LSFT_T(KC_3), LCTL_T(KC_4), KC_5, KC_6, RCTL_T(KC_7), RSFT_T(KC_8), RALT_T(KC_9), KC_0, KC_NO, KC_NO, LCTL(KC_Z), LCTL(KC_Y), LCTL(KC_C), LCTL(KC_V), TD(LOCK_COMPUTER), CW_TOGG, KC_END, KC_PGDN, KC_DOT, QK_DYNAMIC_MACRO_RECORD_START_1, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO),
-    [_LAYER3] = LAYOUT(KC_NO, KC_NO, KC_VOLD, KC_VOLU, KC_MPLY, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_NO, KC_NO, KC_NO, KC_LEFT, LALT_T(KC_DOWN), LSFT_T(KC_UP), LCTL_T(KC_RGHT), KC_PSCR, KC_F11, RCTL_T(KC_F4), RSFT_T(KC_F5), RALT_T(KC_F6), KC_F10, KC_NO, KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, LLOCK, KC_F12, KC_F1, KC_F2, KC_F3, QK_BOOT, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO)
+    [_LAYER0] = LAYOUT(KC_NO, KC_NO, KC_W, KC_F, KC_P, KC_NO, KC_NO, KC_L, KC_U, KC_Y, KC_NO, KC_NO, KC_NO, KC_A, LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G, KC_M, RCTL_T(KC_N), RSFT_T(KC_E), RALT_T(KC_I), KC_O, KC_NO, KC_NO, KC_NO, KC_V, KC_C, KC_D, KC_B, KC_K, KC_H, KC_BSPC, KC_ENT, KC_NO, KC_NO, KC_NO, LTHUMB, KC_NO, KC_NO, RTHUMB, KC_NO),
+    [_LAYER1] = LAYOUT(KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_GRV, KC_NO, KC_NO, KC_DQUO, KC_ASTR, KC_SLSH, KC_NO, KC_NO, KC_NO, KC_QUES, KC_LPRN, KC_RPRN, LCTL_T(KC_SCLN), KC_AT, KC_AMPR, RCTL_T(KC_EQL), KC_PLUS, KC_MINS, KC_EXLM, KC_NO, KC_NO, KC_NO, KC_LCBR, KC_RCBR, KC_COLN, KC_HASH, KC_BSLS, KC_QUOT, KC_COMM, KC_DOT, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO),
+    [_LAYER2] = LAYOUT(KC_NO, KC_NO, ESC_LOCK, KC_DEL, KC_TAB, KC_NO, KC_NO, KC_PLUS, KC_MINS, KC_SLSH, KC_NO, KC_NO, KC_NO, KC_1, LALT_T(KC_2), LSFT_T(KC_3), LCTL_T(KC_4), KC_5, KC_6, RCTL_T(KC_7), RSFT_T(KC_8), RALT_T(KC_9), KC_0, KC_NO, KC_NO, KC_NO, LCTL(KC_Z), LCTL(KC_C), LCTL(KC_V), LCTL(KC_Y), KC_DLR, KC_SPC, KC_BSPC, KC_DOT, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO),
+    [_LAYER3] = LAYOUT(KC_NO, KC_NO, KC_VOLD, KC_VOLU, KC_MPLY, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_NO, KC_NO, KC_NO, KC_LEFT, LALT_T(KC_DOWN), LSFT_T(KC_UP), LCTL_T(KC_RGHT), KC_PSCR, KC_F10, RCTL_T(KC_F4), RSFT_T(KC_F5), RALT_T(KC_F6), LLOCK, KC_NO, KC_NO, KC_NO, KC_PGDN, KC_PGUP, DM_PLY1, DM_REC1, KC_F11, KC_F1, KC_F2, KC_F3, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO)    
 };
 
 
@@ -158,35 +184,36 @@ td_state_t cur_dance(tap_dance_state_t *state) {
         else return TD_SINGLE_HOLD;
     } else if (state->count == 2) {
         if (!state->pressed) return TD_DOUBLE_TAP;
-        else return TD_UNKNOWN;
+        else return TD_DOUBLE_HOLD;
     }
     else return TD_UNKNOWN;
 }
 
-static td_tap_t lc_tap_state = {
+static td_tap_t el_tap_state = {
     .is_press_action = true,
     .state = TD_NONE
 };
-void lc_finished(tap_dance_state_t *state, void *user_data) {
-    lc_tap_state.state = cur_dance(state);
-    switch (lc_tap_state.state) {
-        case TD_SINGLE_TAP:
+void el_finished(tap_dance_state_t *state, void *user_data) {
+    el_tap_state.state = cur_dance(state);
+    switch (el_tap_state.state) {
+        case TD_SINGLE_HOLD:
             tap_code16(LGUI(KC_L));
             break;
-        case TD_DOUBLE_TAP:
+        case TD_DOUBLE_HOLD:
             tap_code16(LGUI(KC_ESC));
             break;
         default:
-            break;
+            tap_code(KC_ESC);
+            break; 
     }
 }
-void lc_reset(tap_dance_state_t *state, void *user_data) {
-    lc_tap_state.state = TD_NONE;
+void el_reset(tap_dance_state_t *state, void *user_data) {
+    el_tap_state.state = TD_NONE;
 }
 
 // Associate our tap dance key with its functionality
 tap_dance_action_t tap_dance_actions[] = {
-    [LOCK_COMPUTER] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lc_finished, lc_reset),
+    [ESC_LOCK] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, el_finished, el_reset),
 };
 
 
@@ -202,6 +229,8 @@ void leader_end_user(void) {
         tap_code16(KC_COLN);
         tap_code(KC_W);
         tap_code(KC_ENT);
+    } else if (leader_sequence_four_keys(KC_B, KC_O, KC_O, KC_T)) {
+        tap_code16(QK_BOOT);
     }
 }
 
@@ -212,6 +241,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     if (!process_layer_lock(keycode, record, LLOCK)) return false;
 
     return true; // Process all other keycodes normally
+}
+
+uint16_t get_combo_term(uint16_t index, combo_t *combo) {
+    switch (combo->keycode) {
+        case KC_X:
+        case KC_Q:
+            return COMBO_TERM + 20; // Increase combo term for ring keys.
+    }
+
+    return COMBO_TERM;
 }
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t* record) {
